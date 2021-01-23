1c3958d6b62c2b8bfad0e77241d0771851adf3be
Andrey Zhaldybin <33570153+Aneryd@users.noreply.github.com>
2021-01-23 15:06:20 +0300

    ЛАБЫ

---
+++ sem3/Первое задание/ent.py
@@ -0,0 +1,24 @@
+import math
+
+mass = []
+k = 1
+
+def file():
+	file_open = open('1.txt')
+	file_print = file_open.read()
+	return file_print
+
+def count(message):
+	file_s = list(message)
+
+	array_d = {}.fromkeys(file_s, 0)
+	for a in file_s:
+		array_d[a] += 1
+	return array_d.values()
+
+def ent(message):
+	return -sum([i/len(message)*math.log2(i/len(message)) for i in count(message)])
+
+text = file()
+print(text)
+print(ent(text))
